d4ff338479d95ffae13433a79c3116a58d66932f
Dhairya <dhairyapop252gmail.com>
2016-05-26 13:09:04 -0700

    Rover Update

--- src/common/Coord.java
+++ src/common/Coord.java
@@ -11,32 +11,32 @@ public class Coord {
 	
 	public final int xpos;
 	public final int ypos;
-	public Terrain terrain;
+    public Terrain terrain;
     public boolean hasRover;
     public Science science;
 	
-    @Override
-    public String toString() {
-        return "Coord [xpos=" + xpos + ", ypos=" + ypos + "]";
+	@Override
+	public String toString() {
+		return "Coord [xpos=" + xpos + ", ypos=" + ypos + "]";
+	}
+
+    public Coord(Terrain terrain, Science science, int x, int y) {
+        this(x, y);
+        this.science = science;
+        this.terrain = terrain;
     }
     
+	public Coord(int x, int y){
+		this.xpos = x;
+		this.ypos = y;
+	}
+	
     /** @return String that can be used to send to other ROVERS. This string
      *         follows the communication protocol: TERRAIN SCIENCE XPOS YPOS
      * @author Shay */
     public String toProtocol() {
         return terrain + " " + science + " " + xpos + " " + ypos;
     }
-
-	public Coord(int x, int y){
-		this.xpos = x;
-		this.ypos = y;
-	}
-	
-	public Coord(Terrain terrain, Science science, int x, int y) {
-		this(x, y);
-		this.science = science;
-		this.terrain = terrain;
-	}
 	
     @Override
     public int hashCode() {
